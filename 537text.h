5e4bcb9ad80961218cd03bcab72ba4963591dbe9
山地奥斯卡537 <131773717+Sean537@users.noreply.github.com>
2023-12-22 22:55:39 +0800

    Add files via upload

---
+++ 537text.h
@@ -0,0 +1,7 @@
+#define CM_FILE_OPEN	9072
+#define CM_FILE_SAVEAS	9071
+#define CM_FILE_EXIT	9070
+#define CM_ABOUT        9069
+#define CM_HELP         9068
+
+#define IDC_MAIN_TEXT 1001
---
+++ 537文本编辑器_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef 537文本编辑器_PRIVATE_H
+#define 537文本编辑器_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"Studio 537"
+#define FILE_VERSION	"1.0.0.0"
+#define FILE_DESCRIPTION	"537文本编辑器"
+#define INTERNAL_NAME	"文本编辑器"
+#define LEGAL_COPYRIGHT	"Copyright @2023 537text"
+#define LEGAL_TRADEMARKS	"537Text"
+#define ORIGINAL_FILENAME	"537text"
+#define PRODUCT_NAME	"537文本编辑器"
+#define PRODUCT_VERSION	"1.0.0.0"
+
+#endif /*537文本编辑器_PRIVATE_H*/
